9832d503ac44e3a9b98f25539397be669ac06a71
andrey.leskov <andrey.leskov@solomoto.com>
2016-09-09 22:04:44 +0300

    adding replay actor

---
+++ GridDomain.Node/EventChronicles/Queries/ReplayActor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka;
+using Akka.Persistence;
+using Akka.Persistence.Query;
+using Akka.Persistence.Query.Sql;
+using Akka.Streams;
+using Akka.Streams.Dsl;
+
+namespace GridDomain.Node.EventChronicles.Queries
+{
+    class ReplayActor<TAggregate> : ReceivePersistentActor
+    {
+        public ReplayActor()
+        {
+            var queries = PersistenceQuery.Get(Context.System).ReadJournalFor<SqlReadJournal>("akka.persistence.query.journal.sql");
+          //  var mat = ActorMaterializer.Create(Context.System);
+            Source<string, NotUsed> src = queries.AllPersistenceIds();
+        }
+
+        public override string PersistenceId => Self.Path.Name;
+    }
+}
